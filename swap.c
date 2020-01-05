32d57cd1cfefd16c6bced85474cf9e997e93aba8
oran2527 <orlago250183@gmail.com>
2020-01-05 00:24:15 +0000

    point 3 swap betty

--- swap.c
+++ swap.c
@@ -6,28 +6,27 @@
  */
 void _s(stack_t **h, unsigned int nl)
 {
-	int i = 0;
-	
-	(void)nl;
+	int i = 0, first = 0, second = 0;
 
-	while(*h != NULL)
+	(void)nl;
+	while (*h != NULL && (*h)->prev != NULL)
 	{
 		*h = (*h)->prev;
 		i++;
 	}
-	printf("swap %d\n", i); 
-	/*
-	if (*h != NULL)
+	i++;
+	while (*h != NULL && (*h)->next != NULL)
+		*h = (*h)->next;
+	if (i < 2)
 	{
-		*h = temp->next;
-		if (temp->next)
-			temp->next->prev = NULL;
-		free(temp);
+		dprintf(STDERR_FILENO, "L%u: can't swap, stack too short", nl);
+		_retexfail();
 	}
 	else
 	{
-		printf("L%u: can't pop an empty stack\n", nl);
-		_retexfail();
+		first = (*h)->n;
+		second = (*h)->prev->n;
+		(*h)->n = second;
+		(*h)->prev->n = first;
 	}
-	*/
 }
